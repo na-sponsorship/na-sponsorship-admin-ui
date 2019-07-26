54c8f8c0e298b0732d67b0e9746b87048db0da61
Eugen Istoc <eugenistoc@gmail.com>
2019-07-26 07:43:35 -0400

    add vuetify

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Noah's Arc</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld />
+      <v-btn absolute dark fab bottom right color="pink">
+        <v-icon>add</v-icon>
+      </v-btn>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data: () => ({
+    //
+  })
+};
+</script>
