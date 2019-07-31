fc503ae325b874ff9e0dde0c3c219d34bbd52f0d
Eugen Istoc <eugenistoc@gmail.com>
2019-07-31 07:44:29 -0400

    fix linting issues

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,3 @@
     <router-view :key="$route.fullPath"></router-view>
   </v-app>
 </template>
-
-<script>
-import ChildList from "./views/child/ChildList.view";
-
-export default {
-  name: "App",
-  components: {
-    ChildList
-  },
-  data: () => ({
-    //
-  })
-};
-</script>
--- src/views/Login.view.vue
+++ src/views/Login.view.vue
@@ -22,7 +22,7 @@ export default {
     };
   },
   methods: {
-    login(credentials) {
+    login() {
       this.$router.replace("/dashboard");
     }
   }
