306bad0ed9cf8e2dc3179eaa6f7aebc2157ebb68
Eugen Istoc <eugenistoc@gmail.com>
2019-10-13 04:53:11 -0400

    Add filepond

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
+import vueFilePond from "vue-filepond";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import axios from "./plugins/axios";
-import vueFilePond from "vue-filepond";
 import Cloudinary from "cloudinary-vue";
 
 import "filepond/dist/filepond.min.css";
